acc8384aef6f29fb664a00b7ae6f21ca1f37ddd6
Lauris <la.stepanovs@gmail.com>
2020-02-08 14:55:42 +0200

    one line fix

--- cloud-functions/secret-erea.js
+++ cloud-functions/secret-erea.js
@@ -1,4 +1,4 @@
-exports.hendler = function(event, context, callback){
+exports.handler = function(event, context, callback){
     callback(null, {
         statusCode:200,
         body:"Welocome to the super secret area!"
